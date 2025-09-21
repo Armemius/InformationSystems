8ab2b7d5c910f369104ba6bf10492de2341020ee
Armemius <rusgy@yandex.ru>
2025-09-22 01:34:35 +0300

    feat(lab-1/backend): add common success response dto
    Add DTO that will be used if there is no return data and it will just
    contain some message

---
+++ lab-1/backend/src/main/java/com/armemius/labwork/data/dto/SimpleSuccessDto.java
@@ -0,0 +1,12 @@
+package com.armemius.labwork.data.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SimpleSuccessDto {
+    private String data = "Success";
+}
