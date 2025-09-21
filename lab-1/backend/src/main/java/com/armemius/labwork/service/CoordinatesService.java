77950710965c78f0096daed097370354db02499f
Armemius <rusgy@yandex.ru>
2025-09-21 22:14:58 +0300

    feat(lab-1/backend): add template services for objects
    Add empty services for all of the objects in subject area

---
+++ lab-1/backend/src/main/java/com/armemius/labwork/service/CoordinatesService.java
@@ -0,0 +1,9 @@
+package com.armemius.labwork.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CoordinatesService {
+}
