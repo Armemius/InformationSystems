ca5c5a4a99f533c75a01bc5ee3b3bfd6a0e4da4d
Armemius <rusgy@yandex.ru>
2025-09-21 16:49:37 +0300

    feat(lab-1/backend): add user retrival to its repository
    Add method to retrieve first user by the username

--- lab-1/backend/src/main/java/com/armemius/labwork/data/repository/AppUserRepository.java
+++ lab-1/backend/src/main/java/com/armemius/labwork/data/repository/AppUserRepository.java
@@ -1,6 +1,12 @@
 package com.armemius.labwork.data.repository;
 
 import com.armemius.labwork.data.domain.AppUser;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AppUserRepository extends JpaRepository<AppUser, Long> {}
+import java.util.Optional;
+
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+    Optional<AppUser> findFirstByUsername(@NotBlank @Size(min = 5, max = 16) String username);
+}
