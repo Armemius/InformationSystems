344d87a3e28668383f57de3b80edd83cbe805c82
Armemius <rusgy@yandex.ru>
2025-09-22 05:38:19 +0300

    feat(lab-1/backend): filter organizations
    Implement filtering by name to organization related logic

--- lab-1/backend/src/main/java/com/armemius/labwork/controller/OrganizationController.java
+++ lab-1/backend/src/main/java/com/armemius/labwork/controller/OrganizationController.java
@@ -4,9 +4,16 @@ import com.armemius.labwork.data.dto.objects.OrganizationDto;
 import com.armemius.labwork.service.crud.OrganizationCrudService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/management/organization")
@@ -14,8 +21,32 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin("*")
 public class OrganizationController extends CrudController<OrganizationDto> {
 
+    private final OrganizationCrudService organizationCrudService;
+
     @Autowired
     protected OrganizationController(OrganizationCrudService crudService) {
         super(crudService);
+        this.organizationCrudService = crudService;
+    }
+
+
+    @Override
+    public ResponseEntity<List<OrganizationDto>> getAll(
+            @RequestParam(defaultValue = "0") Integer page,
+            @RequestParam(defaultValue = "10") Integer size,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "asc") String sort,
+            @RequestParam(required = false) String filter
+    ) {
+        return ResponseEntity.ok(organizationCrudService.getAllWithFilter(
+                PageRequest.of(
+                        page,
+                        size,
+                        Objects.equals(sort, "asc") ?
+                                Sort.by(sortBy).ascending() :
+                                Sort.by(sortBy).descending()
+                ),
+                filter
+        ).toList());
     }
 }
--- lab-1/backend/src/main/java/com/armemius/labwork/service/crud/OrganizationCrudService.java
+++ lab-1/backend/src/main/java/com/armemius/labwork/service/crud/OrganizationCrudService.java
@@ -6,14 +6,28 @@ import com.armemius.labwork.data.repository.OrganizationRepository;
 import com.armemius.labwork.service.abstractions.CrudService;
 import com.armemius.labwork.service.mapper.OrganizationMapperService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.ManagedTypes;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrganizationCrudService extends CrudService<Organization, OrganizationDto> {
 
+    private final OrganizationRepository organizationRepository;
+
     @Autowired
     public OrganizationCrudService(OrganizationRepository repository, OrganizationMapperService mapper) {
         super(repository, mapper);
+        this.organizationRepository = repository;
     }
 
+    public Page<OrganizationDto> getAllWithFilter(PageRequest page, String filter) {
+        if (filter == null || filter.isEmpty()) {
+            return super.getAll(page);
+        }
+        return organizationRepository.findAllByName(filter, page).map(mapper::toDto);
+    }
 }
