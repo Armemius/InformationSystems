7e96e1f38eb60bf253d1e440e3e617254559f6e7
Armemius <rusgy@yandex.ru>
2025-09-26 19:34:36 +0300

    feat(lab-1/frontend): create auth components
    Create set of components related to authorization process

---
+++ lab-1/frontend/src/lib/components/auth/auth-form.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card/index.js';
+
+	import LoginContent from '$lib/components/auth/login-content.svelte';
+	import RegisterContent from '$lib/components/auth/register-content.svelte';
+
+	let isLogin = $state(true);
+
+	const changeMode = () => {
+		isLogin = !isLogin;
+	};
+</script>
+
+<Card.Root class="mx-auto w-full max-w-sm">
+	<Card.Header>
+		<Card.Title class="text-2xl">Вход</Card.Title>
+		<Card.Description>Введите свои данные чтобы продолжить</Card.Description>
+	</Card.Header>
+	<Card.Content>
+		{#if isLogin}
+			<LoginContent {changeMode} />
+		{:else}
+			<RegisterContent {changeMode} />
+		{/if}
+	</Card.Content>
+</Card.Root>
