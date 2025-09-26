ba558ef5b3aa2e7b731c9d6385e8098c43c50c8c
Armemius <rusgy@yandex.ru>
2025-09-26 21:01:01 +0300

    feat(lab-1/frontend): install sidebar template
    Add ShadCN preset for sidebar and edit it to match the project needs

---
+++ lab-1/frontend/src/lib/components/app-sidebar.svelte
@@ -0,0 +1,114 @@
+<script lang="ts" module>
+	import AudioWaveformIcon from '@lucide/svelte/icons/audio-waveform';
+	import BookOpenIcon from '@lucide/svelte/icons/book-open';
+	import BoxIcon from '@lucide/svelte/icons/box';
+	import LinkIcon from '@lucide/svelte/icons/link';
+	import BriefcaseIcon from '@lucide/svelte/icons/briefcase';
+
+	import ChartPieIcon from '@lucide/svelte/icons/chart-pie';
+	import CommandIcon from '@lucide/svelte/icons/command';
+	import FrameIcon from '@lucide/svelte/icons/frame';
+	import GalleryVerticalEndIcon from '@lucide/svelte/icons/gallery-vertical-end';
+	import MapIcon from '@lucide/svelte/icons/map';
+	import Settings2Icon from '@lucide/svelte/icons/settings-2';
+	import SquareTerminalIcon from '@lucide/svelte/icons/square-terminal';
+
+	const data = {
+		navMain: [
+			{
+				title: 'Предметная область',
+				url: '#',
+				icon: BoxIcon,
+				items: [
+					{
+						title: 'Организации',
+						url: '/management/organizations'
+					},
+					{
+						title: 'Координаты',
+						url: '/management/coordinates'
+					},
+					{
+						title: 'Локации',
+						url: '/management/locations'
+					},
+					{
+						title: 'Адреса',
+						url: '/management/addresses'
+					}
+				]
+			},
+			{
+				title: 'Управление',
+				url: '#',
+				icon: BriefcaseIcon,
+				items: [
+					{
+						title: 'Карта',
+						url: '/map'
+					},
+					{
+						title: 'Операции',
+						url: '/operations'
+					},
+				]
+			},
+			{
+				title: 'Исходники',
+				url: '#',
+				icon: LinkIcon,
+				items: [
+					{
+						title: 'Репозиторий с кодом',
+						url: 'https://github.com/Armemius/InformationSystems/'
+					},
+					{
+						title: 'Frontend на GitHub',
+						url: 'https://github.com/Armemius/InformationSystems/tree/main/lab-1/frontend'
+					},
+					{
+						title: 'Backend на GitHub',
+						url: 'https://github.com/Armemius/InformationSystems/tree/main/lab-1/backend'
+					},
+					{
+						title: 'Документация',
+						url: API_URL + 'docs/'
+					}
+				]
+			}
+		]
+	};
+</script>
+
+<script lang="ts">
+	import NavMain from './nav-main.svelte';
+	import NavProjects from './nav-projects.svelte';
+	import NavUser from './nav-user.svelte';
+	import TeamSwitcher from './team-switcher.svelte';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import type { ComponentProps } from 'svelte';
+	import { authStore } from '$lib/stores/user';
+	import { API_URL } from '$lib/config';
+
+	let {
+		ref = $bindable(null),
+		collapsible = 'icon',
+		...restProps
+	}: ComponentProps<typeof Sidebar.Root> = $props();
+</script>
+
+<Sidebar.Root {collapsible} {...restProps}>
+	<Sidebar.Content>
+		<NavMain items={data.navMain} />
+	</Sidebar.Content>
+	<Sidebar.Footer>
+		<NavUser
+			user={{
+				name: $authStore!.username,
+				role: $authStore!.role === 'ROLE_USER' ? 'Пользователь' : 'Админинстратор',
+				avatar: '/avatars/shadcn.jpg'
+			}}
+		/>
+	</Sidebar.Footer>
+	<Sidebar.Rail />
+</Sidebar.Root>
---
+++ lab-1/frontend/src/lib/components/nav-user.svelte
@@ -0,0 +1,86 @@
+<script lang="ts">
+	import * as Avatar from '$lib/components/ui/avatar/index.js';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import { useSidebar } from '$lib/components/ui/sidebar/index.js';
+	import { clearUser } from '$lib/stores/user';
+	import BadgeCheckIcon from '@lucide/svelte/icons/badge-check';
+	import BellIcon from '@lucide/svelte/icons/bell';
+	import ChevronsUpDownIcon from '@lucide/svelte/icons/chevrons-up-down';
+	import CreditCardIcon from '@lucide/svelte/icons/credit-card';
+	import LogOutIcon from '@lucide/svelte/icons/log-out';
+	import SparklesIcon from '@lucide/svelte/icons/sparkles';
+	import SunIcon from '@lucide/svelte/icons/sun';
+	import MoonIcon from '@lucide/svelte/icons/moon';
+
+	import { toggleMode } from 'mode-watcher';
+
+	let { user }: { user: { name: string; role: string; avatar: string } } = $props();
+	const sidebar = useSidebar();
+</script>
+
+<Sidebar.Menu>
+	<Sidebar.MenuItem>
+		<DropdownMenu.Root>
+			<DropdownMenu.Trigger>
+				{#snippet child({ props })}
+					<Sidebar.MenuButton
+						size="lg"
+						class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+						{...props}
+					>
+						<Avatar.Root class="size-8 rounded-lg">
+							<Avatar.Image src={user.avatar} alt={user.name} />
+							<Avatar.Fallback class="rounded-lg">CN</Avatar.Fallback>
+						</Avatar.Root>
+						<div class="grid flex-1 text-left text-sm leading-tight">
+							<span class="truncate font-medium">{user.name}</span>
+							<span class="truncate text-xs">{user.role}</span>
+						</div>
+						<ChevronsUpDownIcon class="ml-auto size-4" />
+					</Sidebar.MenuButton>
+				{/snippet}
+			</DropdownMenu.Trigger>
+			<DropdownMenu.Content
+				class="w-(--bits-dropdown-menu-anchor-width) min-w-56 rounded-lg"
+				side={sidebar.isMobile ? 'bottom' : 'right'}
+				align="end"
+				sideOffset={4}
+			>
+				<DropdownMenu.Label class="p-0 font-normal">
+					<div class="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+						<Avatar.Root class="size-8 rounded-lg">
+							<Avatar.Image src={user.avatar} alt={user.name} />
+							<Avatar.Fallback class="rounded-lg">CN</Avatar.Fallback>
+						</Avatar.Root>
+						<div class="grid flex-1 text-left text-sm leading-tight">
+							<span class="truncate font-medium">{user.name}</span>
+							<span class="truncate text-xs">{user.role}</span>
+						</div>
+					</div>
+				</DropdownMenu.Label>
+				<DropdownMenu.Separator />
+				<DropdownMenu.Group>
+					<DropdownMenu.Item onclick={toggleMode}>
+						<SunIcon
+							class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 !transition-all dark:scale-0 dark:-rotate-90"
+						/>
+						<MoonIcon
+							class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 !transition-all dark:scale-100 dark:rotate-0"
+						/>
+						Сменить тему
+					</DropdownMenu.Item>
+					<!-- <DropdownMenu.Item>
+						<SparklesIcon />
+						Стать админинстратором
+					</DropdownMenu.Item> -->
+				</DropdownMenu.Group>
+				<DropdownMenu.Separator />
+				<DropdownMenu.Item onclick={clearUser}>
+					<LogOutIcon />
+					Выйти
+				</DropdownMenu.Item>
+			</DropdownMenu.Content>
+		</DropdownMenu.Root>
+	</Sidebar.MenuItem>
+</Sidebar.Menu>
